64f839a728473ec5f1374b1c1d57aab7894ec148
blackawa <3104kurokawa@gmail.com>
2017-12-15 13:09:24 +0900

    :recycle: rename module

--- auth/docker/postgresql/init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE auth_test;
-    GRANT ALL PRIVILEGES ON DATABASE auth_test TO ${POSTGRES_USER};
-EOSQL
