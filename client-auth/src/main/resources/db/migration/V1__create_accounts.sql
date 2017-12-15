2c3dabd6cfc753db1fecf093550824b9a9034e91
blackawa <3104kurokawa@gmail.com>
2017-12-15 15:24:47 +0900

    :seedling: create tables

---
+++ client-auth/src/main/resources/db/migration/V1__create_accounts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE accounts (
+  id       SERIAL PRIMARY KEY,
+  email    VARCHAR(256) NOT NULL UNIQUE,
+  password VARCHAR(256) NOT NULL
+);
---
+++ client-auth/src/main/resources/db/migration/V3__create_roles.sql
@@ -0,0 +1,7 @@
+CREATE TABLE roles (
+  account_id INTEGER NOT NULL,
+  role_code_id    INTEGER NOT NULL,
+  PRIMARY KEY (account_id, role_code_id),
+  FOREIGN KEY (account_id) REFERENCES accounts(id),
+  FOREIGN KEY (role_code_id) REFERENCES role_codes(id)
+);
